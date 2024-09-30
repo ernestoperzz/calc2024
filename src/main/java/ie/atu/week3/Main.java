ac02d0a1d09c548c38d2bff2d9084cdc0021ff4b
ERNESTOALONSOPEREZ-S <g00436110@atu.ie>
2024-09-30 10:05:43 +0100

    updated code

--- src/main/java/ie/atu/week3/Main.java
+++ src/main/java/ie/atu/week3/Main.java
@@ -1,7 +1,39 @@
 package ie.atu.week3;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
 
-}
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner scan1= new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        double firstNum = scan1.nextDouble();
+        System.out.println("You entered "+ firstNum);
+
+
+        System.out.println("Please enter a second number: ");
+        double secondNum = scan1.nextDouble();
+        System.out.println("You entered " + secondNum);
+
+        String operation;
+        System.out.println("Enter the option");
+        operation = scan1.next();
+
+        //Ask the user for operation
+
+        boolean validOperation =true;
+        switch(operation){
+            case "add":
+                add(firstNum,secondNum);
+                break;
+            default:
+                System.out.println("Invalid operation. ");
+                break;
+        }
+    }
+
+
+
+
+
+
